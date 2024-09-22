e89a8e443f1f7c1eb26191c78b62741466589a7f
jc <2326671686@qq.com>
2024-09-22 16:20:52 +0800

    数据库

--- config/database.php
+++ config/database.php
@@ -4,10 +4,10 @@ declare (strict_types=1);
 return [
     'driver' => 'mysql',
     'host' => '127.0.0.1',
-    'database' => 'demo',
-    'username' => 'demo',
-    'password' => 'TbfXmL2JTcXYYrWZ',
+    'database' => 'faka',
+    'username' => 'root',
+    'password' => '123456',
     'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
+    'collation' => 'utf8mb4_general_ci',
     'prefix' => 'acg_',
 ];
